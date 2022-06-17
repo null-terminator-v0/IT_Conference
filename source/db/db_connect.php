47820381f37a14c8d32968e48ac87ea4145a76bf
KeeganK <keegan.kinnear@affinityfuneral.co.za>
2022-06-17 15:53:08 +0200

    attempting to connect to the database

--- source/db/db_connect.php
+++ source/db/db_connect.php
@@ -15,7 +15,7 @@
                 $this->db = new PDO($this->user, $this->password, $dsn);
             }catch (PDOException $e){
                 echo "<h1 class='text-danger'>Failed to connect to Database</h1>";
-                throw new PDOException($e->getMessage());
+                throw new $e->getMessage();
             }
         }
 
