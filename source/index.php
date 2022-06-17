093300173ae658d8ce3333f7cd2a766b7fcd1333
KeeganK <keegan.kinnear@affinityfuneral.co.za>
2022-06-17 14:03:02 +0200

    changing sub-header text color

--- source/index.php
+++ source/index.php
@@ -4,9 +4,9 @@ require_once "includes/header.php";
 ?>
 
     <br>
-    <h1 class="display-4 text-center"><u>IT Conference</u></h1>
+    <h1 class="display-4 text-center text-secondary"><u>IT Conference</u></h1>
     <br>
-    <h1 class="display-6  text-center text-secondary">Registration Page</h1>
+    <h1 class="display-6  text-center">Registration Page</h1>
     <br>
 
     <form>
