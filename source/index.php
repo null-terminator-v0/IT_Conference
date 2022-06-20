3c8ee0efa95e5cbb8a7299ef5721373a303c2ade
KeeganK <keegan.kinnear@affinityfuneral.co.za>
2022-06-20 09:03:58 +0200

    adding a modal triggered by the submit button

--- source/index.php
+++ source/index.php
@@ -32,10 +32,31 @@
             <input type="text" class="form-control" id="email" aria-describedby="emailHelp" autocomplete="off" required>
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
+        <!--    Button to submit sign up information and trigger modal to determine whether the data was saved into the database   -->
         <div class="d-grid gap-2">
-            <button type="submit" id="submit" class="btn btn-dark">Submit</button>
+            <button type="submit" id="submit" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#modal">Submit</button>
         </div>
     </form>
+
+    <!-- Modal to determine whether data was saved into the database    -->
+    <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
 <?php require_once "includes/footer.php"; ?>
