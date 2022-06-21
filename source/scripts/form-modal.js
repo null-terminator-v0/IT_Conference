1801ef34fb6910fc3d33756886365cb19a743d12
KeeganK <keegan.kinnear@affinityfuneral.co.za>
2022-06-21 10:03:30 +0200

    attaching jquery

--- source/scripts/form-modal.js
+++ source/scripts/form-modal.js
@@ -1,35 +1,30 @@
-$(document).ready(function () {
-    $("#submit").click(function () {
-        $("a").hide();
-    });
-});
-
-$(document).ready(function () {
-    $("button").dblclick(function () {
-        $(this).hide();
-    });
-    $("input").click(function () {
-        $(this).hide();
-    });
-    $(".form-select").click(function () {
-        $(this).hide();
-    });
-});
+// $(document).ready(function () {
+//     $("#submit").click(function () {
+//         $("a").hide();
+//     });
+// });
+//
+// $(document).ready(function () {
+//     $("button").dblclick(function () {
+//         $(this).hide();
+//     });
+//     $("input").click(function () {
+//         $(this).hide();
+//     });
+//     $(".form-select").click(function () {
+//         $(this).hide();
+//     });
+// });
 
 $(document).ready(function () {
         $("input").on(
             mouseenter: function () {
                 $(this).css("background-color", "orange");
-            };
-        );
-        $("input").on(
+            },
             mouseleace: function(){
                 $(this).css("background-color", "lightblue");
-            };
-        );
-        $("input").on(
+            },
             dblclick: function () {
                 $(this).css("background-color", "black");
-            }
-        );
+            },
 });
